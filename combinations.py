a561bb5ee2187902caf86effd5003d97416f460e
theoportlock <zn.tportlock@gmail.com>
2020-06-17 00:23:39 +0100

    Big cleanup

--- combinations.py
+++ combinations.py
@@ -11,5 +11,5 @@ def a(arr):
 if __name__ == "__main__":
     import IO
     in_file = "input.bin"
-    out_file =  "combinations.bin"
+    out_file = "combinations.bin"
     IO.arr2file(a(IO.file2arr(in_file)),out_file)

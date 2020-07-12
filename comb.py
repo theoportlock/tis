ba026f61f860c637c079ff25cc848dd52ce26e37
theoportlock <zn.tportlock@gmail.com>
2020-07-12 15:09:33 +0100

    Fixes the scientific form key converter with sets

--- comb.py
+++ comb.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 def a(input_array,memory_array):
     import numpy as np
-    from itertools import combinations
+    import powerset
     '''Takes an array of binary digits and returns an array of all combinations of that array'''
     converted_input = []
     for i in range(input_array.bit_length()):
@@ -9,21 +9,17 @@ def a(input_array,memory_array):
         if 2**i & input_array == 2**i:
             converted_input.append(2**i)
 
-<<<<<<< HEAD
-    for j,k in enumerate(converted_input):
-        # find the active combination and add one to the charge
-        if j & input_array == j:
-=======
     converted_input_np = np.array(converted_input)
-    for j in np.combinations(converted_input_np):
-        if np.sum(j) & input_array == j:
->>>>>>> 90eedb1dd2d5e4b92ca94d8f5925e2e50b86b0c3
-            ind = "{:.50e}".format(j)
-            memory_array[ind] = memory_array.get(ind, [j, 0.0])
+    for j in powerset.a(converted_input_np):
+        k = sum(j)
+        if k & input_array == k:
+            ind = "{:.50e}".format(k)
+            memory_array[ind] = memory_array.get(ind, [k, 0.0])
             memory_array[ind][1] += 1
-            memory_array[ind][0] = j
+            memory_array[ind][0] = k
     print(memory_array)
     return memory_array
+
 '''
 array[array1 & array2 = array1]
 '''
--- documents/recruitment/compile.sh
+++ documents/recruitment/compile.sh
@@ -2,6 +2,4 @@
 fullnam="recruitment.tex"
 nam=$(echo $fullnam | cut -f 1 -d  '.')
 pdflatex -shell-escape $fullnam && 
-bibtex $nam && 
-pdflatex -shell-escape $fullnam && 
 pdflatex -shell-escape $fullnam &

c20ecfc002d760d36e4eb1569c4780b805e55120
theoportlock <zn.tportlock@gmail.com>
2020-08-03 19:31:28 +0100

    Improves testing
    Need to work out how to manage sparcity

--- main.py
+++ main.py
@@ -3,34 +3,40 @@ import IO
 import functions as f
 import sys
 
+
 def comb(input_int):
-    '''Takes an array of binary digits and returns an array of all combinations of that array'''
+    ''' Takes an integer, converts to binary, finds predicted bits based on a
+    memory file, then updates a "memory" and "predict" file
+    '''
 
-    # Create memory if one doesn't exist and load
-    memory_array = IO.mem2set()
+    # create memory if one doesn't exist and load
+    memory_list = IO.mem2set()
 
-    # Finds all combinations of active bits in an integer
-    predict = 0
-    for j in f.powerset(f.convert(input_int)):
-        k = sum(j)
-        if k & input_int == k:
-            memory_array.add(k)
-            for l in memory_array:
-                if k & l == k:
-                    predict |= (l - k)
-                    '''
-                    print("k = ", bin(k)[:1:-1])
-                    print("l = ", bin(l)[:1:-1])
-                    print("l-k=", bin(l-k)[:1:-1])
-                    print("out=", bin(predict)[:1:-1])
-                    '''
-
-    predict -= (predict & input_int)
+    predict_int = 0
+    # finds all combinations of active bits in an integer
+    for comb_list in f.powerset(f.convert(input_int)):
+        comb_int = sum(comb_list)
+        memory_list.add(comb_int)
+        # if the combination is found in memory, predict the difference
+        for mem_int in memory_list:
+            if comb_int & mem_int == comb_int:
+                predict_int |= (mem_int - comb_int)
+                '''
+                print("comb_int = ", bin(comb_int)[:1:-1])
+                print("mem_int  = ", bin(mem_int)[:1:-1])
+                print("mem-comb = ", bin(mem_int-comb_int)[:1:-1])
+                print("out=       ", bin(predict_int)[:1:-1])
+                '''
+    # for difference:  predict_int -= (predict_int & input_int)
+    '''
     print("inp=", bin(input_int)[2:])
-    print("pred", bin(predict)[2:])
+    print("pred", bin(predict_int)[2:])
+    '''
 
-    IO.set2file(memory_array, "memory")
-    IO.int2text(predict, "predict")
+    # save the prediction and memory
+    IO.set2file(memory_list, "memory")
+    IO.int2text(predict_int, "predict")
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:

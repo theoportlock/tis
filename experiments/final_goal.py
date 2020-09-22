4fc7a0c80276aefb09272a114be207af92129191
Theo Portlock <NMRbox.org support@nmrbox.org>
2020-09-22 09:52:04 -0400

    Finished experiments setup

---
+++ experiments/final_goal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import unittest
+import os
+from src import IO
+from src import functions as f
+from src.main import comb
+
+
+def test_ultimate_goal(self):
+    # In order to affect sparceness, it is necessary to convert some of the active
+    # bits to 0 from the input string.  This, I believe is done incorrectly by
+    # traditional HTM architectures where that effect is mandated manually by
+    # external preprocessing algorythms. Conversely, the reverse process of input compression is also necessary.
+    df = pd.DataFrame(columns=["inp","pre"])
+    data = "Hello Jello"
+    with open(self.testing_files["inp"], 'w') as of: of.write(data)
+    for i in f.concat(f.basechanger(IO.textfile2int(self.testing_files["inp"]), 8), 4):
+        self.worker.load(files=self.testing_files, from_text=True)
+        self.worker.inp = i
+        self.worker.run().save(files=self.testing_files, text=True)
+        with open("results", 'a') as of: of.write(bin(self.worker.pre)[:2:-1] + "\n")
+        df.loc[i] = [bin(self.worker.inp)[:2:-1], bin(self.worker.pre)[:2:-1]]
+        df.to_csv("outdf.csv")
+        df.plot()
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import unittest
 import os
 from src import IO
@@ -59,19 +60,6 @@ class TestMain(unittest.TestCase):
             self.assertTrue(os.path.isfile(self.testing_files["pre"]))
 
 '''
-    def test_big_text(self):
-        df = pd.DataFrame(columns=["inp","pre"])
-        data = "Hello World"
-        with open(self.testing_files["inp"], 'w') as of: of.write(data)
-        converted_integer = f.basechanger(IO.textfile2int(self.testing_files["inp"]), 50)
-        for i in f.concat(converted_integer, 4):
-            self.worker.load(files=self.testing_files, from_text=True)
-            self.worker.inp = i
-            self.worker.run().save(files=self.testing_files, text=True)
-            with open("results", 'a') as of: of.write(bin(self.worker.pre)[:2:-1] + "\n")
-            df.loc[i] = [bin(self.worker.inp)[:2:-1], bin(self.worker.pre)[:2:-1]]
-        df.to_csv("outdf.csv")
-
     def test_ultimate_goal(self):
         # In order to affect sparceness, it is necessary to convert some of the active
         # bits to 0 from the input string.  This, I believe is done incorrectly by

9462ed507fa819ccc3b343436f28ea60d0a14585
theoportlock <zn.tportlock@gmail.com>
2021-03-09 11:12:03 +0000

    Better parrot

--- Experiments/parrot/parrot.py
+++ Experiments/parrot/parrot.py
@@ -12,12 +12,12 @@ import sys
 steve = worker()
 start_time = str(datetime.now().strftime("%H:%M:%S"))
 
-with open(f"Experiments/parrot/{start_time}", mode="w+") as csv_file:
-    csv_file.writelines("time, inp, bininp, pre, binpre")
+with open(f"Experiments/parrot/{start_time}.csv", mode="w+") as csv_file:
+    csv_file.writelines("time, inp, bininp, pre, binpre\n")
 
 while True:
     time_now = datetime.now().strftime("%H:%M:%S")
-    d1 = [0]*20 + [1]*10
+    d1 = [0]*12 + [1]*3
     random.shuffle(d1)
     bininp = "".join(str(i) for i in d1)
     inp = int(bininp[::-1], 2)
@@ -28,6 +28,8 @@ while True:
     pre = steve.pre
     binpre = bin(steve.pre)[:1:-1]
 
-    with open(f"Experiments/parrot/{start_time}", mode="w+") as csv_file:
+    with open(f"Experiments/parrot/{start_time}.csv", mode="a") as csv_file:
         time_now = datetime.now().strftime("%H:%M:%S")
-        csv_file.writelines(f"{time_now}, {inp}, {bininp}, {pre}, {binpre}")
+        out = f"{time_now}, {inp}, {bininp}, {pre}, {binpre}\n"
+        print(out)
+        csv_file.writelines(out)
--- runexperiment.sh
+++ runexperiment.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 #name=$(basename $(ls Experiments/* | fzf))
 
 name="parrot.py"
+trap "[ ! -e $name ] || rm $name " EXIT
+
 cp Experiments/$(basename $name .py)/$name . 
 echo "$(date): $name begin"
-timeout 5000 python $name
+timeout 50 python $name
 echo "$(date): $name done"

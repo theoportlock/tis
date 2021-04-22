7abb75858d5a3d0919305933756beabe77791d32
theoportlock <zn.tportlock@gmail.com>
2021-04-22 15:25:03 +0100

    Minor edits

--- Experiments/parrot/parrot.py
+++ Experiments/parrot/parrot.py
@@ -12,17 +12,18 @@ steve = worker()
 
 print("time, inp, bininp, pre, binpre")
 while True:
-    d1 = [0]*10 + [1]*2
-    random.shuffle(d1)
-    bininp = "".join(str(i) for i in d1)
-    inp = int(bininp[::-1], 2)
+    for i in range(15):
+        d1 = [0]*i + [1]*2
+        random.shuffle(d1)
+        bininp = "".join(str(i) for i in d1)
+        inp = int(bininp[::-1], 2)
 
-    steve.inp = inp
-    steve.run()
-    steve.predict()
+        steve.inp = inp
+        steve.run()
+        steve.predict()
 
-    pre = steve.pre
-    binpre = bin(steve.pre)[:1:-1]
-    time_now = datetime.now().strftime("%H:%M:%S")
+        pre = steve.pre
+        binpre = bin(steve.pre)[:1:-1]
+        time_now = datetime.now().strftime("%H:%M:%S")
 
-    print(f"{time_now}, {inp}, {bininp}, {pre}, {binpre}")
+        print(f"{time_now}, {inp}, {bininp}, {pre}, {binpre}")

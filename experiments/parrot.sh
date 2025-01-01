6a1dee176c4827fcc61d5615ffce91ff7aecd94a
theoportlock <zn.tportlock@gmail.com>
2025-01-01 13:59:28 -0800

    Parrot function is first experiment

--- experiments/parrot.sh
+++ experiments/parrot.sh
@@ -2,5 +2,10 @@ export PATH=$PATH:../src/
 write.py 10011 -o ../data/input
 write.py 0 -o ../data/memory
 memorize.py
-read.py -f ../data/memory
 read.py
+read.py -f ../data/memory
+
+write.py 10111 -o ../data/input
+memorize.py
+read.py
+read.py -f ../data/memory

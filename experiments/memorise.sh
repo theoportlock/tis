532af04d1316601f452cd0ae7754b91e28fa743d
theoportlock <theo.portlock@auckland.ac.nz>
2026-04-03 12:18:44 +0100

    Thinking about the event loop

--- experiments/memorise.sh
+++ experiments/memorise.sh
@@ -4,7 +4,7 @@ source env.sh
 write.py -i 10011 -o work/input
 write.py -i 0 -o work/memory
 
-memorize.py -i work/input -m work/memory -o work/memory1
+memor.py -i work/input -m work/memory -o work/memory1
 
 read.py -i work/input
 read.py -i work/memory
